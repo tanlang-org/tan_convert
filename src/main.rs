3084e9d286d0a8b7b79152c9421d199af0cb7424
George Moschovitis <gmosx@reizu.com>
2023-01-07 13:44:28 +0200

    Wip

--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,29 @@ fn main() -> anyhow::Result<()> {
         .version(VERSION)
         .about("A CLI for converting Tan text and binary files from/to other formats")
         .arg(
-            Arg::new("FROM")
-                .help("The path of the file to convert from")
+            Arg::new("INPUT")
+                .help("The path of the input file")
                 .required(true)
                 .index(1),
         )
         .arg(
-            Arg::new("TO")
-                .help("The path of the file to convert to")
+            Arg::new("OUTPUT")
+                .help("The path of the output file")
                 .required(true)
                 .index(2),
         );
 
     let matches = cmd.get_matches();
 
-    dbg!(&matches);
+    let input_path: &String = matches
+        .get_one("INPUT")
+        .expect("missing path to the input file");
+
+    let output_path: &String = matches
+        .get_one("OUTPUT")
+        .expect("missing path to the output file");
+
+    dbg!(input_path, output_path);
 
     Ok(())
 }
