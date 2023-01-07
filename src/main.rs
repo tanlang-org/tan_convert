54ef766b71ad07ca29969609cd8698976f9958e5
George Moschovitis <gmosx@reizu.com>
2023-01-07 18:20:38 +0200

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,32 +8,6 @@ use tan::{
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-// #TODO visitor pattern
-// #TODO Expr.fold, no need, will get it for free from an Expr iterator.
-// #TODO this is the visitor pattern.
-// fn traverse<E>(expr: E, f: Rc<dyn Fn(&Expr)>)
-// where
-//     E: AsRef<Expr>,
-// {
-//     let expr = expr.as_ref();
-
-//     match expr {
-//         Expr::Array(exprs) => {
-//             for x in exprs {
-//                 // #TODO ARRRGGhhh, Rc!!
-//                 traverse(x, f.clone());
-//             }
-//         }
-//         Expr::List(exprs) => {
-//             for x in exprs {
-//                 // #TODO ARRRGGhhh, Rc!!
-//                 traverse(x, f.clone());
-//             }
-//         }
-//         _ => f(expr),
-//     }
-// }
-
 fn expr_to_json<E>(expr: E) -> Value
 where
     E: AsRef<Expr>,
